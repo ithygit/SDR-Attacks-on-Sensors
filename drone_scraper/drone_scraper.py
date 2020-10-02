7275a34bb761611dac3b56b8b1bd973febf6ef6b
Jianqiu Cao <jianqiu_cao@163.com>
2020-10-01 18:31:59 -1000

    Changed python scripts and added NMEA file holmes6.txt.

--- drone_scraper/drone_scraper.py
+++ drone_scraper/drone_scraper.py
@@ -11,7 +11,8 @@ This is a temporary script file.
 import csv
 from pymavlink import mavutil
 import keyboard
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from read_plot import plot_gps
 
 # Start a connection listening to a UDP port
 the_connection = mavutil.mavlink_connection('udpin:192.168.99.2:14550')
@@ -26,7 +27,9 @@ print("Heartbeat from system (system %u component %u)" % (the_connection.target_
 csv_filename = 'drone_status.csv'
 with open(csv_filename, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
-    field = ['time', 'fix_type', 'EKF_status','lat', 'lon', 'alt', 'vel_acc', 'h_acc', 'v_acc']
+    field = ['time', 'fix_type', 'EKF_status','lat', 'lon', 'alt', 'vel_acc', 'h_acc', 'v_acc', 
+             'vel', 'global_lat', 'global_lon', 'global_alt', 'global_vx', 'global_vy', 'global_vz', 
+             'HUD_airspeed', 'HUD_groundspeed']
     csvwriter.writerow(field);
     print(field)
     
@@ -45,29 +48,51 @@ with open(csv_filename, 'w', newline='') as csvfile:
         h_acc = the_connection.messages['GPS_RAW_INT'].h_acc # Position uncertainty in mm
         v_acc = the_connection.messages['GPS_RAW_INT'].v_acc # Altitude uncertainty in mm
         
+        # New variables
+        vel = the_connection.messages['GPS_RAW_INT'].vel # GPS ground speed in cm/s
+        global_lat = the_connection.messages['GLOBAL_POSITION_INT'].lat # Fused global position in degE7
+        global_lon = the_connection.messages['GLOBAL_POSITION_INT'].lon
+        global_alt = the_connection.messages['GLOBAL_POSITION_INT'].alt
+        global_vx = the_connection.messages['GLOBAL_POSITION_INT'].vx # Ground Speed in cm/s
+        global_vy = the_connection.messages['GLOBAL_POSITION_INT'].vy
+        global_vz = the_connection.messages['GLOBAL_POSITION_INT'].vz
+        HUD_airspeed = the_connection.messages['VFR_HUD'].airspeed # m/s
+        HUD_groundspeed = the_connection.messages['VFR_HUD'].groundspeed
+        # x_vel = the_connection.messages['CONTROL_SYSTEM_STATE'].x_vel # velocity in body frame
+        # y_vel = the_connection.messages['CONTROL_SYSTEM_STATE'].y_vel
+        # z_vel = the_connection.messages['CONTROL_SYSTEM_STATE'].z_vel
+        # x_pos = the_connection.messages['CONTROL_SYSTEM_STATE'].x_pos
+        # y_pos = the_connection.messages['CONTROL_SYSTEM_STATE'].y_pos
+        # z_pos = the_connection.messages['CONTROL_SYSTEM_STATE'].z_pos
+        # airspeed = the_connection.messages['CONTROL_SYSTEM_STATE'].airspeed
+        
         time_s = time_usec / 1000000
         lat_deg = lat / 10000000
         lon_deg = lon / 10000000
-        alt_deg = alt / 10000000
+        alt_deg = alt / 1000
         vel_acc_m = vel_acc / 1000
         h_acc_m = h_acc / 1000
         v_acc_m = v_acc / 1000
         
+        # New variables
+        vel_m_s = vel / 100
+        global_lat_deg = global_lat / 10000000
+        global_lon_deg = global_lon / 10000000
+        global_alt_deg = global_alt / 1000
+        global_vx_m_s = global_vx / 100
+        global_vy_m_s = global_vy / 100
+        global_vz_m_s = global_vz / 100
+        
         lat_list.append(lat_deg)
         lon_list.append(lon_deg)
         
-        row = [time_s, fix_type, ekf_status, lat_deg, lon_deg, alt_deg, vel_acc_m, h_acc_m, v_acc_m]
+        row = [time_s, fix_type, ekf_status, lat_deg, lon_deg, alt_deg, vel_acc_m, h_acc_m, v_acc_m,
+               vel_m_s, global_lat_deg, global_lon_deg, global_alt_deg, global_vx_m_s, global_vy_m_s, global_vz_m_s, 
+               HUD_airspeed, HUD_groundspeed]
         csvwriter.writerow(row);
         print(row)
     
-    # time.sleep(0.25)
-    fig = plt.figure(figsize=(16,9))
-    plt.title('Drone trajectory')
-    plt.xlabel('Longitude(deg)')
-    plt.ylabel('Latitude(deg)')
-    #plt.xticks(np.arange(50, 160, 10.0))
-    plt.grid(b=True, which='both')
-    plt.scatter(x=lon_list, y=lat_list);
-    plt.plot(lon_list[0], lat_list[0], 'gs', markersize = 12);
-    plt.plot(lon_list[-1], lat_list[-1], 'r^', markersize = 12);
+        # time.sleep(0.25)
+        
+plot_gps(csv_filename, 'holmes.txt')
     
--- drone_scraper/read_plot.py
+++ drone_scraper/read_plot.py
@@ -7,6 +7,7 @@ Created on Fri Sep 18 16:28:29 2020
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 import utm
 
 def lat2meter(lat):
@@ -37,53 +38,110 @@ def meter2lon(meter):
     except:
         return meter
 
+def plot_gps(gps_file, trajectory_file):
+    df = pd.read_csv (gps_file)
+    # fig, ax = plt.subplots()
+    fig = plt.figure(constrained_layout=True)
+    plt.title('Drone trajectory')
+    plt.xlabel('X (meter)')
+    plt.ylabel('Y (meter)')
+    plt.grid(b=True, which='both')
+    # plt.ticklabel_format(useOffset=False)
+    plt.ticklabel_format(style='plain')
+    plt.axis('equal')
+    
+    # Plot GPS points
+    meter1_x, meter1_y, _, _ = utm.from_latlon(df['lat'].values, df['lon'].values)
+    # Calculate the start and end points, and the utm parameters
+    global start_x, start_y, end_x, end_y, easting, northing, zone_number, zone_letter
+    start_x = meter1_x[0]
+    start_y = meter1_y[0]
+    end_x = meter1_x[-1]
+    end_y = meter1_y[-1]
+    easting, northing, zone_number, zone_letter = utm.from_latlon(df['lat'].values[0], df['lon'].values[0])
+    # Plot the trajectory
+    scatter1 = plt.scatter(x=meter1_x, y=meter1_y, c=df['fix_type'], cmap='Paired', zorder=2)
+    # Plot arrows
+    # plt.quiver(x[:-1], y[:-1], x[1:]-x[:-1], y[1:]-y[:-1], scale_units='xy', angles='xy', scale=1, width=0.001, zorder=3, color='blue')
+    plt.quiver(meter1_x[:-1], meter1_y[:-1], meter1_x[1:]-meter1_x[:-1], meter1_y[1:]-meter1_y[:-1], scale_units='xy', angles='xy', scale=1, width=0.001, zorder=3, color='blue')
+    # Start point
+    start_point, = plt.plot(meter1_x[0], meter1_y[0], 'gs', zorder=1, markersize=10)
+     # End point
+    end_point, = plt.plot(meter1_x[-1], meter1_y[-1], 'r^', zorder=1, markersize=10)
+    
+    # Plot the original spoofing trajectory
+    # df2 = pd.read_csv ('lowercampus.txt', header=None)
+    df2 = pd.read_csv (trajectory_file, header=None)
+    lat2 = (df2[2]//100 + df2[2]%100/60) * ((df2[3]=="N") * 2 - 1)
+    lon2 = (df2[4]//100 + df2[4]%100/60) * ((df2[5]=="E") * 2 - 1)
+    meter2_x, meter2_y, _, _ = utm.from_latlon(lat2.values, lon2.values)
+    scatter2 = plt.scatter(x=meter2_x, y=meter2_y, marker='*', s=10, zorder=3)
+    plt.plot(meter2_x[0], meter2_y[0], 'gs', zorder=1, markersize=10)
+    plt.plot(meter2_x[-1], meter2_y[-1], 'r^', zorder=1, markersize=10)
+    
+    # # Plot the GPS points from global position
+    # global_meter1_x, global_meter1_y, _, _ = utm.from_latlon(df['global_lat'].values, df['global_lon'].values)
+    # scatter3 = plt.scatter(x=global_meter1_x, y=global_meter1_y, c=df['fix_type'],  zorder=2)    
+    # # Plot arrows    
+    # plt.quiver(global_meter1_x[:-1], global_meter1_y[:-1], global_meter1_x[1:]-global_meter1_x[:-1], global_meter1_y[1:]-global_meter1_y[:-1], scale_units='xy', angles='xy', scale=1, width=0.001, zorder=3, color='red')
+    
+    # Create the legend
+    legend1 = plt.legend((start_point, end_point, scatter2), ('Start point', 'End point', 'Original trajectory'), loc = 'upper right')
+    classes = ['No fix', '3D fix']
+    plt.gca().add_artist(legend1)
+    legend2 = plt.legend(handles=scatter1.legend_elements()[0], labels=classes, title = 'Drone trajectory', loc = 'lower right')
+    
+    # Secondary axes
+    ax = plt.gca()
+    secaxx = ax.secondary_xaxis('top', functions=(meter2lon, lon2meter))
+    secaxx.set_xlabel('Longitude(deg)')
+    secaxy = ax.secondary_yaxis('right', functions=(meter2lat, lat2meter))
+    secaxy.set_ylabel('Latitude(deg)')
+    
+    
+    # Plot the fix type and EKF status
+    time = df['time'].values - df['time'].values[0]
+    fig2, axs = plt.subplots(3)
+    # fig2.tight_layout()
+    axs[0].plot(time, df['fix_type'], '--', color='blue')
+    axs[0].set_title('Fix type and EKF Status')
+    # axs[0].set_xlabel('Time')
+    axs[0].xaxis.set_minor_locator(MultipleLocator(10))
+    axs[0].set_ylabel('Fix type', color='blue')
+    axs[0].tick_params(axis='y', labelcolor='blue')
+    axs[0].set_yticks([0, 1, 2, 3])
+    axs[0].set_yticklabels(['0 - No GPS', '1 - No fix', '', '3 - 3D fix'])
+    ax0b = axs[0].twinx()
+    ax0b.plot(time, df['EKF_status'], '-', color='orange')
+    ax0b.set_ylabel('EKF Status', color='orange')
+    ax0b.tick_params(axis='y', labelcolor='orange')
+    # axs[0].legend()
+    # Plot accuracy
+    axs[1].plot(time, df['h_acc'], label = 'Position Accuracy')
+    axs[1].plot(time, df['vel_acc'], label = "Velocity Accuracy")
+    axs[1].set_title('Position and Velocity Accuracy')
+    axs[1].xaxis.set_minor_locator(MultipleLocator(10))
+    axs[1].set_ylabel('(m)')
+    axs[1].set_ylim(0,100)
+    axs[1].legend()
+    # Plot speed
+    axs[2].plot(time, df['vel'], '-', label = 'Ground Speed from GPS_RAW_INT')
+    vx = df['global_vx'].values
+    vy = df['global_vy'].values
+    vz = df['global_vz'].values
+    speed = (vx**2 + vy**2 + vz**2) ** .5
+    axs[2].plot(time, speed, ':', label = 'Ground Speed from GLOBAL_POSITION_INT ')
+    axs[2].plot(time, df['HUD_groundspeed'], '--', label = 'Ground Speed from HUD')
+    axs[2].plot(time, df['HUD_airspeed'], '-.', label = 'Air Speed from HUD')
+    axs[2].set_title('GPS Ground Speed and Air Speed')
+    axs[2].set_xlabel('Time')
+    axs[2].xaxis.set_minor_locator(MultipleLocator(10))
+    axs[2].set_ylabel('(m/s)')
+    axs[2].set_ylim(0,0.5)
+    axs[2].legend(loc = 'upper right')
+
 # Plot the drone GPS trajectory
-# df = pd.read_csv ('drone_status1.csv')
-df = pd.read_csv ('drone_status2.csv')
+# plot_gps ('drone_status1.csv', 'lowercampus.txt')
+# plot_gps ('drone_status2.csv')
+plot_gps ('drone_status6.csv', 'holmes6.txt')
 
-# fig, ax = plt.subplots()
-fig = plt.figure(constrained_layout=True)
-plt.title('Drone trajectory')
-plt.xlabel('X (meter)')
-plt.ylabel('Y (meter)')
-plt.grid(b=True, which='both')
-# plt.ticklabel_format(useOffset=False)
-plt.ticklabel_format(style='plain')
-plt.axis('equal')
-
-# Plot GPS points
-meter1_x, meter1_y, _, _ = utm.from_latlon(df['lat'].values, df['lon'].values)
-scatter1 = plt.scatter(x=meter1_x, y=meter1_y, c=df['fix_type'], cmap='Paired', zorder=2)
-start_x = meter1_x[0]
-start_y = meter1_y[0]
-end_x = meter1_x[-1]
-end_y = meter1_y[-1]
-easting, northing, zone_number, zone_letter = utm.from_latlon(df['lat'].values[0], df['lon'].values[0])
-# Plot arrows
-# plt.quiver(x[:-1], y[:-1], x[1:]-x[:-1], y[1:]-y[:-1], scale_units='xy', angles='xy', scale=1, width=0.001, zorder=3, color='blue')
-plt.quiver(meter1_x[:-1], meter1_y[:-1], meter1_x[1:]-meter1_x[:-1], meter1_y[1:]-meter1_y[:-1], scale_units='xy', angles='xy', scale=1, width=0.001, zorder=3, color='blue')
-# Starting point
-starting_point, = plt.plot(meter1_x[0], meter1_y[0], 'gs', zorder=1, markersize=10)
- # End point
-end_point, = plt.plot(meter1_x[-1], meter1_y[-1], 'r^', zorder=1, markersize=10)
-
-# Plot the original spoofing trajectory
-df2 = pd.read_csv ('lowercampus.txt', header=None)
-lat2 = (df2[2]//100 + df2[2]%100/60) * ((df2[3]=="N") * 2 - 1)
-lon2 = (df2[4]//100 + df2[4]%100/60) * ((df2[5]=="E") * 2 - 1)
-meter2_x, meter2_y, _, _ = utm.from_latlon(lat2.values, lon2.values)
-scatter2 = plt.scatter(x=meter2_x, y=meter2_y, marker='*', s=10, zorder=3)
-plt.plot(meter2_x[0], meter2_y[0], 'gs', zorder=1, markersize=10)
-plt.plot(meter2_x[-1], meter2_y[-1], 'r^', zorder=1, markersize=10)
-
-legend1 = plt.legend((starting_point, end_point, scatter2), ('Starting point', 'End point', 'Original trajectory'), loc = 'upper right')
-classes = ['No fix', '3D fix']
-plt.gca().add_artist(legend1)
-legend2 = plt.legend(handles=scatter1.legend_elements()[0], labels=classes, title = 'Drone trajectory', loc = 'lower right')
-
-# Secondary axes
-ax = plt.gca()
-secaxx = ax.secondary_xaxis('top', functions=(meter2lon, lon2meter))
-secaxx.set_xlabel('Longitude(deg)')
-secaxy = ax.secondary_yaxis('right', functions=(meter2lat, lat2meter))
-secaxy.set_ylabel('Latitude(deg)')
